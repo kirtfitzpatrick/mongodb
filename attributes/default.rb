519226e62b5a424b207f240b5bf38d37b763a4f5
Kirt Fitzpatrick <kirt.fitzpatrick@gmail.com>
2013-03-01 15:40:10 -0800

    changed the db_path to an attribute and changed the default replicaset name to rs_default

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,8 @@ default[:mongodb][:port]      = 27017
 default[:mongodb][:verbose]   = false
 default[:mongodb][:journal]   = true
 
+default[:mongodb][:dbpath] = "/var/lib/mongodb"
+
 # Change initial_nodes to the number of nodes you plan to spin up in 
 # parallel so the don't all try to make themselves the master simultaneously
 default[:mongodb][:replset][:initial_nodes]    = 3
@@ -10,7 +12,7 @@ default[:mongodb][:replset][:initial_nodes]    = 3
 # Set the replset:name attribute to a unique value amongst all the nodes on 
 # your chef server.  This attribute will be used to find all the other nodes 
 # to build the replica set.
-default[:mongodb][:replset][:name]             = "default"
+default[:mongodb][:replset][:name]             = "rs_default"
 
 # If you want the node to be an arbiter, set this to true.
 default[:mongodb][:replset][:arbiter]          = false
